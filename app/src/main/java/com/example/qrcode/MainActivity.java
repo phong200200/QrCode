0a716b5d98459210e725f9eff6d89ad2c4f72bc3
phong200200 <quocphong20022000@gmail.com>
2021-05-16 18:18:47 +0700

    Created Qr by string

--- app/src/main/java/com/example/qrcode/MainActivity.java
+++ app/src/main/java/com/example/qrcode/MainActivity.java
@@ -2,16 +2,58 @@ package com.example.qrcode;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
 
 public class MainActivity extends AppCompatActivity {
 
+
+        //add core-3.2.1 to ..\app\libs
+        //implementation group: 'com.google.zxing', name: 'core', version: '3.2.1'
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView imv = (ImageView) findViewById(R.id.imvQR);
+        try {
+            Bitmap bitmap = encodeAsBitmap("https://github.com/phong200200/QrCode.git");
+            imv.setImageBitmap(bitmap);
+        }catch (WriterException e){
+            e.printStackTrace();
+        }
 
 
-
+    }
+    Bitmap encodeAsBitmap(String str) throws WriterException {
+        BitMatrix result;
+        try {
+            result = new MultiFormatWriter().encode(str,
+                    BarcodeFormat.QR_CODE, 512, 512, null);
+        } catch (IllegalArgumentException iae) {
+            // Unsupported format
+            return null;
+        }
+        int w = result.getWidth();
+        int h = result.getHeight();
+        int[] pixels = new int[w * h];
+        for (int y = 0; y < h; y++) {
+            int offset = y * w;
+            for (int x = 0; x < w; x++) {
+                pixels[offset + x] = result.get(x, y) ? Color.BLACK : Color.WHITE;
+            }
+        }
+        Bitmap bitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        bitmap.setPixels(pixels, 0, 512, 0, 0, w, h);
+        return bitmap;
     }
 }
